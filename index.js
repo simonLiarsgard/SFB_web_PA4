dfa8328fbac35c1e3dbbcb5956e66ed979cb2bfe
Simon <simon@smaforetagsbyran.se>
2020-03-19 10:51:59 +0100

    setup2

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 //Express setup
 const app = express();
 
+app.get('/'), function() {
+    console.log('GET request');
+};
+
 //Listen for requests
 app.listen(process.env.port || 4000, function (){
     console.log('now listening for requests');
